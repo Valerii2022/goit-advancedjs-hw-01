d3ab8f0542bc901b14250c9dece99286d8375dc0
Valerii Pometun <vmpometun@gmail.com>
2023-11-01 12:02:21 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,26 +4,31 @@ const emailRef = document.querySelector('input');
 const messageRef = document.querySelector('textarea');
 const formRef = document.querySelector('.feedback-form');
 
-const currentUserInfo = JSON.parse(localStorage.getItem('feedback-form-state'));
+const LOCALSTORAGE_FORM_KEY = 'feedback-form-state';
+
+const currentUserInfo = JSON.parse(localStorage.getItem(LOCALSTORAGE_FORM_KEY));
 const userInfo = currentUserInfo || { name: '', message: '' };
 
 emailRef.value = userInfo.name;
 messageRef.value = userInfo.message;
 
+emailRef.required = true;
+messageRef.required = true;
+
 const handleEmailInputChange = e => {
   userInfo.name = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+  localStorage.setItem(LOCALSTORAGE_FORM_KEY, JSON.stringify(userInfo));
 };
 
 const handleMessagelInputChange = e => {
   userInfo.message = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+  localStorage.setItem(LOCALSTORAGE_FORM_KEY, JSON.stringify(userInfo));
 };
 
 const handleSubmitForm = e => {
   e.preventDefault();
   console.log(userInfo);
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(LOCALSTORAGE_FORM_KEY);
   formRef.reset();
 };
 
