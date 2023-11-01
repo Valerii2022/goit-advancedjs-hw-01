4fee6f6abf588ec69a0c315dc4620e005ee0f210
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-11-01 23:40:43 +0200

    update homework files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const iframeRef = document.querySelector('iframe');
+
+const player = new Player(iframeRef);
+
+const LOCALSTORAGE_VIDEOPLAYER_KEY = 'videoplayer-current-time';
+
+const currentTime = JSON.parse(
+  localStorage.getItem(LOCALSTORAGE_VIDEOPLAYER_KEY)
+);
 
 const onPlay = e => {
-  localStorage.setItem('videoplayer-current-time', e.seconds);
+  localStorage.setItem(LOCALSTORAGE_VIDEOPLAYER_KEY, e.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
-
-const currentTime = JSON.parse(
-  localStorage.getItem('videoplayer-current-time')
-);
 
 player
   .setCurrentTime(currentTime)
@@ -20,5 +23,12 @@ player
     console.log(seconds);
   })
   .catch(function (error) {
-    console.log(error.name);
+    switch (error.name) {
+      case 'RangeError':
+        console.log(error.name);
+        break;
+
+      default:
+        break;
+    }
   });
